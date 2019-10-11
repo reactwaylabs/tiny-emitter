5dca703634f5b7c46331c4086ca61635feae69b5
Martynas Žilinskas <martynas@quatrodev.com>
2019-10-11 21:02:12 +0300

    Refactored tiny-emitter.

--- src/tiny-emitter.ts
+++ src/tiny-emitter.ts
@@ -1,65 +1,30 @@
-// tslint:disable-next-line:no-any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Callback = (...args: any[]) => void;
 
-export class TinyEmitter<THandler extends Callback> {
-    private registry?: Callback | Callback[];
+export class TinyEmitter<THandler extends Callback = () => void> {
+    private registry: Callback[] = [];
 
     public addListener(handler: THandler): () => void {
-        if (this.registry == null) {
-            this.registry = handler;
-        } else if (Array.isArray(this.registry)) {
-            this.registry.push(handler);
-        } else {
-            this.registry = [this.registry, handler];
-        }
+        this.registry.push(handler);
 
         return () => this.removeListener(handler);
     }
 
     public removeListener(handler: THandler): void {
-        if (this.registry == null) {
+        if (this.registry.length === 0) {
             return;
         }
 
-        if (Array.isArray(this.registry)) {
-            const nextArray: Callback[] = [];
-
-            for (let i = 0; i < this.registry.length; i++) {
-                const callback = this.registry[i];
-                if (callback !== handler) {
-                    nextArray.push(callback);
-                }
-            }
-
-            this.registry = nextArray;
-        } else {
-            this.registry = undefined;
-        }
+        this.registry = this.registry.filter(handlerItem => handlerItem !== handler);
     }
 
     public emit(...payload: Parameters<THandler>): void {
-        if (this.registry == null) {
-            return;
-        }
-
-        if (Array.isArray(this.registry)) {
-            for (let i = 0; i < this.registry.length; i++) {
-                this.registry[i](...payload);
-            }
-        } else {
-            this.registry(...payload);
+        for (const handler of this.registry) {
+            handler(...payload);
         }
     }
 
     public getListenersCount(): number {
-        if (this.registry == null) {
-            return 0;
-        }
-
-        if (Array.isArray(this.registry)) {
-            return this.registry.length;
-        } else {
-            return 1;
-        }
+        return this.registry.length;
     }
 }
